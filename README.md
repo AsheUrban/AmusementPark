0319d20b008acf0f69a92c81580d7b34cb41abee
Ashe Urban <ashe@goldentongue.com>
2023-01-11 16:56:21 -0800

    Add and Update README.md
    co-authored by: Mesha Devan <github madsketchz@gmail.com>
    Co-authored-by: Mitchell Gantz <mgantz22@icloud.com>

---
+++ README.md
@@ -0,0 +1,36 @@
+# Amusement Park - Branching in JavaScript
+
+#### A basic webpage that uses if else statements.
+
+#### By Ashe Urban, Mesha, & Mitchell Ganz
+
+## Technologies Used
+
+* _css_
+* _html_
+* _markdown_
+* _js_
+
+## Description
+
+_Sometimes, our branching logic can get more complicated. Let's build a website to determine which amusement park ride someone can go on based on their age and height. In the process, we'll learn about using the logical "and" && and "or" || operators, nesting if statements, and the concepts of "truthy" and "falsey". We'll also include polish our website by adding error handling and a button to clear the form and results._
+
+_Link to assignment:_ https://www.learnhowtoprogram.com/introduction-to-programming/javascript-and-web-browsers/more-branching-and-error-handling
+
+## Setup/Installation Requirements
+
+* _clone or download responsitory to your local_
+
+## Known Bugs
+
+* _Incomplete project_
+
+## License
+
+* MIT
+
+## Contact Information
+
+_Please contact me with any questions or contribuitions, ashe@goldentongue.com_
+
+Copyright(c) 2023 Ashe Urban, Mesha, & Mitchell
--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
 function hideResults(){
-    document.getElementById("swings").setAttritbute("class","hidden");
+    document.getElementById("swings").setAttribute("class","hidden");
     document.getElementById("coaster").setAttribute("class","hidden");
-    document.getElementById("tower").setAttritbute("class","hidden");
-    document.getElementById("sorry").setAttritbute("class","hidden");
+    document.getElementById("tower").setAttribute("class","hidden");
+    document.getElementById("sorry").setAttribute("class","hidden");
 }
 
-window.onload = function() {
-    document.querySelector("form").onsubmit = function(event) {
+window.onload = function () {
+  document.querySelector("form").onsubmit = function (event) {
     event.preventDefault();
     hideResults();
     const age = parseInt(document.querySelector("input#age").value);
-    const height = parseInt(document.querySelector("input#height").value);     
-    };
+    const height = parseInt(document.querySelector("input#height").value);
+  };
+
+  // add if statement to call to remove attribute for class per lesson 61, starting at "Adding the First Condition -- Tower of Doom"
 };
